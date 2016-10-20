e9f0fc3f0f71cb4a5839f8a6134e9bb8c18c8292
Mary Martinez <mlmartinez85@gmail.com>
2016-10-20 09:41:56 -0700

    Setup settings view controller

---
+++ GithubDemo/SettingsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsTableViewController.swift
+//  GithubDemo
+//
+//  Created by Mary Martinez on 10/20/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
